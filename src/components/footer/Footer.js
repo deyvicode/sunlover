fde444de23dad0e6938ca17e527649cf697e348d
Deyvi De La cruz <deyvicode@gmail.com>
2021-12-19 15:25:44 -0500

    change class to ClassName

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,21 +2,21 @@ import { NavLink } from "react-router-dom"
 
 const Footer = ({ menuLinks }) => {
     return (
-        <footer class="footer section">
-            <div class="footer__container bd-grid">
-                <div class="footer__box">
-                    <h3 class="footer__title">SunLover</h3>
-                    <p class="footer__description">Summer Collection</p>
+        <footer className="footer section">
+            <div className="footer__container bd-grid">
+                <div className="footer__box">
+                    <h3 className="footer__title">SunLover</h3>
+                    <p className="footer__description">Summer Collection</p>
                 </div>
                 
-                <div class="footer__box">
-                    <h3 class="footer__title">EXPORAR</h3>
+                <div className="footer__box">
+                    <h3 className="footer__title">EXPORAR</h3>
                     <ul>
                         {
                             menuLinks.map((element, index) => {
                                 return (
                                     <li key={index}>
-                                        <NavLink to={element.url} class="footer__link">{element.name}</NavLink>
+                                        <NavLink to={element.url} className="footer__link">{element.name}</NavLink>
                                     </li>
                                 )
                             })
@@ -24,24 +24,24 @@ const Footer = ({ menuLinks }) => {
                     </ul>
                 </div>
         
-                <div class="footer__box">
-                    <h3 class="footer__title">SOPORTE</h3>
+                <div className="footer__box">
+                    <h3 className="footer__title">SOPORTE</h3>
                     <ul>
-                        <li><a href="/" class="footer__link">Consulta por productos</a></li>
-                        <li><a href="/" class="footer__link">Ayuda en la compra</a></li>
-                        <li><a href="/" class="footer__link">Devoluciones y Reclamos</a></li>
+                        <li><a href="/" className="footer__link">Consulta por productos</a></li>
+                        <li><a href="/" className="footer__link">Ayuda en la compra</a></li>
+                        <li><a href="/" className="footer__link">Devoluciones y Reclamos</a></li>
                     </ul>
                 </div>
         
-                <div class="footer__box ">
-                    <a href="/" class="footer__social"><i class='bx bxl-facebook'></i></a>
-                    <a href="/" class="footer__social"><i class='bx bxl-instagram'></i></a>
-                    <a href="/" class="footer__social"><i class='bx bxl-twitter'></i></a>
-                    <a href="/" class="footer__social"><i class='bx bxl-google'></i></a>
+                <div className="footer__box ">
+                    <a href="/" className="footer__social"><i className='bx bxl-facebook'></i></a>
+                    <a href="/" className="footer__social"><i className='bx bxl-instagram'></i></a>
+                    <a href="/" className="footer__social"><i className='bx bxl-twitter'></i></a>
+                    <a href="/" className="footer__social"><i className='bx bxl-google'></i></a>
                 </div>
             </div>
 
-            <p class="footer__copy">2021 &#169; <a href="https://www.linkedin.com/in/deyvidelacruz/" target="_blank" rel="noreferrer">Deyvi De La Cruz</a>. All right reserved.</p>
+            <p className="footer__copy">2021 &#169; <a href="https://www.linkedin.com/in/deyvidelacruz/" target="_blank" rel="noreferrer">Deyvi De La Cruz</a>. All right reserved.</p>
         </footer>
     )
 }
