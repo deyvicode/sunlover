bbd71386ca9565ecf8205841da6ebf96d1190b7d
Deyvi De La cruz <deyvicode@gmail.com>
2022-01-29 09:44:15 -0500

    env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDg-V2zadFHmRLCzt8yfuXPSM6seqqvXQE",
-    authDomain: "sunlover-cfe53.firebaseapp.com",
-    projectId: "sunlover-cfe53",
-    storageBucket: "sunlover-cfe53.appspot.com",
-    messagingSenderId: "170559061076",
-    appId: "1:170559061076:web:d49cc46c18b76265b601c1"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
